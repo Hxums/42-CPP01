204be00a56ecb8edca4431ddd7182d0017d8d3e3
hcissoko <hcissoko@student.42.fr>
2026-06-29 18:04:00 +0200

    ex06 done

--- ex06/Harl.cpp
+++ ex06/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: hcissoko <hcissoko@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/28 20:16:41 by hcissoko          #+#    #+#             */
-/*   Updated: 2026/06/28 21:49:15 by hcissoko         ###   ########.fr       */
+/*   Updated: 2026/06/29 18:03:18 by hcissoko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,16 @@ Harl::~Harl(void){}
 
 void	Harl::debug(void)
 {
-	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!\n" << std::endl;
 }	
 void	Harl::info(void)
 {
-	std::cout << "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!" << std::endl;
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!\n" << std::endl;
 }
 
 void	Harl::warning(void)
 {
-	std::cout << "I think I deserve to have some extra bacon for free. I've been coming for years, whereas you started working here just last month." << std::endl;
+	std::cout << "I think I deserve to have some extra bacon for free. I've been coming for years, whereas you started working here just last month.\n" << std::endl;
 }
 
 void	Harl::error(void)
@@ -50,18 +50,22 @@ void	Harl::complain(std::string level)
 	{
 		case 0:
 			(this->*functions[0])();
+			std::cout << "[" << levels[0] << "]" << std::endl;
 			__attribute__ ((fallthrough));
 		case 1:
+			std::cout << "[" << levels[1] << "]" << std::endl;
 			(this->*functions[1])();
 			__attribute__ ((fallthrough));
 		case 2:
+			std::cout << "[" << levels[2] << "]" << std::endl;
 			(this->*functions[2])();
 			[[fallthrough]];
 		case 3:
+			std::cout << "[" << levels[3] << "]" << std::endl;
 			(this->*functions[3])();
 			break;
 		default:
+			std::cout << "Probably complaining about insignificant problem" << std::endl;
 			break;
 	}
-	std::cout << "Probably complaining about insignificant problem" << std::endl;
 }
